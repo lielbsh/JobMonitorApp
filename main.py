fa2289a1a3992d6b6c46324ce564a9ba7be62cd7
lielbsh <liel1939@gmail.com>
2025-06-09 21:32:59 +0300

    chore: create SQLAlchemy engine and session management

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from db.init_db import init_db
 from services.gmail_reader import authenticate_gmail, get_messages_gmail, process_gmail_messages
 
 def main():
+    init_db()
     gmail = authenticate_gmail()
     messages = get_messages_gmail(gmail)
     process_gmail_messages(messages, gmail)
