9d743278dac300566dac8b3142bbc847e86aa12f
lielbsh <liel1939@gmail.com>
2025-06-18 22:00:18 +0300

    refactor: rename and update run_ingestion_pipeline to process messages individually

--- ingestion.py
+++ ingestion.py
@@ -1,7 +1,9 @@
+import os
 import time
 from config import BOOTSTRAP_QUERY, RUN_QUERY_TEMPLATE
 from db.init_db import init_db
-from services.gmail_fetcher import authenticate_gmail, get_messages_gmail, process_gmail_messages
+import settings
+from services.gmail_fetcher import authenticate_gmail, get_messages_gmail, process_gmail_message
 import logging
 import argparse
 
@@ -10,11 +12,37 @@ logging.basicConfig(
     format="%(asctime)s | %(levelname)s | %(message)s"
 )
 
-def run_ingestion_pipeline(query: str):
+def run_ingestion_pipeline_locally(query: str):
     init_db()
     gmail = authenticate_gmail()
     messages = get_messages_gmail(service=gmail, query=query)
-    process_gmail_messages(messages, gmail)
+
+    for idx, message in enumerate(messages, start=1):
+        gmail_id = message['id']
+
+        if not IS_LAMBDA:
+            from db.crud import email_exist, insert_email, update_or_create_job
+            if email_exist(gmail_id):
+                return
+
+        job_data, message_data = process_gmail_message(idx, message, service=gmail, gmail_id=gmail_id)
+
+        if job_data.get("status") == "Not Relevant":
+            insert_email(
+            message_data=message_data,
+            job_id=None
+            )
+            continue
+
+        # Saves to db  
+        job_id = update_or_create_job(job_data, message_data)
+        if not job_id:
+            continue
+        
+        insert_email(
+            message_data=message_data,
+            job_id=job_id
+        )
     logging.info("✅ Ingestion completed.")
 
    
@@ -24,4 +52,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     query = RUN_QUERY_TEMPLATE.format(last_checked_ts=int(time.time() - 60 * 60 * 24)) if args.mode == 'run' else BOOTSTRAP_QUERY
-    run_ingestion_pipeline(query=query)
+    run_ingestion_pipeline_locally(query=query)
